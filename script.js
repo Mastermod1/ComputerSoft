3f71b1aa15349a79916f11c7edd967cc1fb3646b
Mastermod1 <danielkalmucki2@gmail.com>
2022-07-20 17:15:57 +0200

    Inicjalizacja i polecenie

---
+++ script.js
@@ -0,0 +1,9 @@
+/* Zadanie 1
+Napisz skrypt, który będzie mógł być „doklejany" do dowolnego dokumentu html. Powinien
+umożliwiać podanie przez użytkownika nazwy elementu lub jego klasy css, a następnie wyświetlać
+treść wszystkich wystąpień tego elementu w dokumencie (DOM).
+Zadanie powinno być wykonany przy użyciu czystego języka JavaScript.
+Po wejściu na stronę (index.html) skrypt automatycznie się włącza, np. alert box pozwalający użytkownikowi na wpisanie klasy css lub nazwy elementu DOM (.class, p, div).
+Skrypt po zatwierdzeniu przez użytkownika alertu powinien wyświetlić wszystkie wystąpienia danych elementów.
+Wyświetlanie wyniku jest dowolne (np. console.log, kolejny alert).
+*/
